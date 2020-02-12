72c96afa809c3de3a4ea845f2e62a3d561f076e1
DiaZa13 <54074539+DiaZa13@users.noreply.github.com>
2020-02-12 16:11:19 -0600

    Renonbrar clase principal

---
+++ Sorts.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.lang.Comparable;
+import java.util.Arrays;
+import java.util.Scanner;
+
+import static java.lang.Integer.*;
+
+
+/**
+ * 
+ * @author Diana Zaray Corado #191025
+ * @version 11/02/2020
+ *
+ */
+public class Sorts {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Integer[] numbers = new Integer[3000];
+		String s = "", cadLista = "";
+		int n;
+		File file = new File();
+		sortingAlgorithms sort = new sortingAlgorithms();
+		//file.orderNumbers();
+		//file.messyNumbers();
+
+		cadLista = file.readFile();
+		String[] arreglo = cadLista.split(" ");
+		for(int i = 0; i< arreglo.length;i++) {
+			int r = Integer.parseInt(arreglo[i]);
+			numbers[i] = r;
+		}
+		System.out.println(Arrays.toString(numbers));
+		System.out.println(Arrays.toString(sort.insertionSort(numbers)));
+
+
+	}
+
+}
